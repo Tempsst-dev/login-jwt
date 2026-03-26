852a6f9a9e44c5d8dab8c18a39f79d92fa58b371
yassir <tempestyassir507@gmail.com>
2026-03-26 14:13:33 +0100

    new back-end project with JWT and bcrypt 1.0.0

---
+++ server.py
@@ -0,0 +1,108 @@
+# ------------------------------    IMPORT MODULES       ------------------------------------------#
+
+from fastapi import FastAPI , Header,HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+import bcrypt
+from jose import JWTError , jwt
+from datetime import datetime,timedelta
+from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
+
+# ------------------------------ CORSMiddleware  ------------------------------------------#
+app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# ------------------------------ server start  ------------------------------------------#
+
+@app.get("/")
+async def root():
+    return {"server":"start"}
+
+# ------------------------------  VARIABLE   ------------------------------------------#
+load_dotenv()
+# ===== DATA BASE=====
+URL=os.getenv("URL")
+CLIENT=MongoClient(URL)
+CREAT_USERS=CLIENT["CREAT_USERS"]
+USER_DATA=CREAT_USERS["USER_DATA"]
+
+# =========== J.W.T ==============
+
+SECRET_KEY=os.getenv("SECRET_KEY")
+
+# ----------------CALSS DATA-------------------
+class Data(BaseModel):
+    name:str
+    lastname:str
+    email:str
+    password:str
+# ---------------------------------  DATA BASE CONECTE   ---------------------------------#
+@app.post("/register")
+async def register(data:Data):
+    password= data.password
+    Bytes=password.encode("utf-8")
+    Salt=bcrypt.gensalt()
+    hash=bcrypt.hashpw(Bytes,Salt)
+    regester_data={
+        "name":data.name,
+        "lastname":data.lastname,
+        "email":data.email,
+        "password":hash
+    }
+ 
+    USER_DATA.insert_one(regester_data)
+
+    return{"message":"data found"}
+
+# ----------------------------      LOG IN      --------------------------
+
+@app.post("/login")
+async def login(data:Data):
+    user=USER_DATA.find_one({"email":data.email})
+    if not user :
+        raise HTTPException(status_code=400,detail="data not found")
+    if not bcrypt.checkpw(data.password.encode("utf-8"),user["password"]):
+        raise HTTPException(status_code=401 , detail="password failed")
+    token_data = {
+        "user_id":str(user["_id"]),
+        "name":user["name"],
+        "lastname":user["lastname"],
+        "email":user["email"],
+        "exp":datetime.utcnow() + timedelta(minutes=60)
+    }
+    token= jwt.encode(token_data,SECRET_KEY,algorithm="HS256")
+    if not token:
+        raise HTTPException(
+            status_code=404,
+            detail="data token not found"
+        )
+    return{"token":token}
+
+# ----------------------------       PROFILE      --------------------------
+
+@app.get("/profile")
+async def profile(authorization:str = Header(None)):
+    if not authorization :
+        raise HTTPException(
+            status_code=401,
+            detail="authoriation not found"
+        )
+    parts  = authorization.split(" ")
+
+    if len(parts ) != 2 or parts[0] != "Bearer":
+        raise HTTPException(status_code=401)
+    token=parts[1]
+    try:
+        decode= jwt.decode(token,SECRET_KEY,algorithms=["HS256"])
+        return{"message": decode}
+    except JWTError:
+        raise HTTPException(status_code=401,detail="data not found")
